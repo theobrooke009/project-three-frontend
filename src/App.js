73adf373379cc8ab7bfdcbe18fe46a187776d344
Tomas <hedberg.tomas@gmail.com>
2021-08-20 12:44:23 +0200

    tomas-changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Nav from './components/common/Navbar.js'
+import Home from './components/common/Home'
+import Movies from './components/Movies.js'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
