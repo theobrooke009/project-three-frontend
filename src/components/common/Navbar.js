0415b3445c43c7ae43f4c88e17c95405b20ff512
Tomas <hedberg.tomas@gmail.com>
2021-08-23 12:19:40 +0200

    dummy-removals-new

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,45 +1,45 @@
-import React from 'react'
-import { Link, useHistory } from 'react-router-dom'
-import { isAuthenticated, removeToken } from '../lib/auth'
+// import React from 'react'
+// import { Link, useHistory } from 'react-router-dom'
+// import { isAuthenticated, removeToken } from '../lib/auth'
 
-function Navbar() {
+// function Navbar() {
 
-  const isAuth = isAuthenticated()
-  const history = useHistory()
+//   const isAuth = isAuthenticated()
+//   const history = useHistory()
 
-  const handleLogout = () => {
-    removeToken()
-    history.push('/')
-  }
+//   const handleLogout = () => {
+//     removeToken()
+//     history.push('/')
+//   }
 
-  return (
-    <navbar>
-      <div className="navbar is-dark">
-        <div className="navbar-container">
-          <div className="navbar-item logo"> 
-            <img src="../components/images/binge-white.png"></img>
-            <div className="buttons">
-              {!isAuth && (
-                <>
-                  <Link to="/register" exact className="button is-danger">
-                  Register
-                  </Link>
-                  <Link to="/login" exact className="button is-danger">
-                  Login
-                  </Link>
-                </>
-              )}
-              {isAuth && (
-                <button className="button is-warning" onClick={handleLogout}>
-                  Log Out
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </navbar>
-  )
-}
+//   return (
+//     <navbar>
+//       <div className="navbar is-dark">
+//         <div className="navbar-container">
+//           <div className="navbar-item logo"> 
+//             <img src="../components/images/binge-white.png"></img>
+//             <div className="buttons">
+//               {!isAuth && (
+//                 <>
+//                   <Link to="/register" exact className="button is-danger">
+//                   Register
+//                   </Link>
+//                   <Link to="/login" exact className="button is-danger">
+//                   Login
+//                   </Link>
+//                 </>
+//               )}
+//               {isAuth && (
+//                 <button className="button is-warning" onClick={handleLogout}>
+//                   Log Out
+//                 </button>
+//               )}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </navbar>
+//   )
+// }
 
-export default Navbar
+// export default Navbar
